76a75fa4df3b1575146c72a925007394332a8ceb
Muhammad Sohaib <58735769+muhammadsohaib60@users.noreply.github.com>
2023-11-28 20:56:15 +0500

    Delete code/src/scene2 directory

--- code/src/scene2/common/trainer/save_reason.py
+++
@@ -1,9 +0,0 @@
-import enum
-
-
-class SaveReason(enum.Enum):
-    """
-    Specify the reason to save a checkpoint
-    """
-    BEST_MODEL = "best_model"
-    INTERVAL = "interval"
--- code/src/scene2/texture_gen/custom_transforms/hsv_transforms.py
+++
@@ -1,9 +0,0 @@
-class ToHSV(object):
-    """
-    TorchVision transform module that converts an image to HSV format.
-    """
-    def __init__(self):
-        pass
-
-    def __call__(self, img):
-        return img.convert("HSV")
